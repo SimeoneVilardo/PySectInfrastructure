b15ae52180502854bc890857aba0c8bd1d063992
Simeone Vilardo <simeone.vilardo@gmail.com>
2024-07-04 00:26:23 +0200

    update script

--- backend-challenge/docker/django/local_run.sh
+++ backend-challenge/docker/django/local_run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-source /opt/django/pypkgs/bin/activate
-python -Wd manage.py runserver 0.0.0.0:8000
-readonly cmd="$*"
-exec $cmd
+if [ -f /opt/django/pypkgs/bin/activate ]; then
+    . /opt/django/pypkgs/bin/activate
+    python -Wd manage.py runserver 0.0.0.0:8000
+else
+    echo "Virtual environment not found!"
+    exit 1
+fi
