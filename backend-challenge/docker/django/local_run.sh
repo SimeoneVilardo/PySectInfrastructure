2d1142ce28fe9b673c463fb6b5d11cbb79b5efca
Simeone Vilardo <simeone.vilardo@gmail.com>
2024-07-03 23:59:58 +0200

    use command inline

--- backend-challenge/docker/django/local_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Startup code for django server used by the docker-compose.
-
-source /opt/django/pypkgs/bin/activate
-python -Wd manage.py runserver 0.0.0.0:8000
-readonly cmd="$*"
-exec $cmd
